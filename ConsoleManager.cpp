e654808dfe90678270b37ca045cfac25084b60d3
Locat <115922986+loc156@users.noreply.github.com>
2025-08-31 22:35:37 +0800

    Add files via upload

---
+++ ConsoleManager.cpp
@@ -0,0 +1,352 @@
+#include "ConsoleManager.h"
+
+using namespace ConsoleFactory;
+
+HANDLE ConsoleManager::getConsoleHandle() {
+	return hConsole;
+}
+
+void ConsoleManager::loadConsoleInfo() {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		throw std::exception("Invalid console handle");
+	}
+	if (!GetConsoleScreenBufferInfo(hConsole, &csbi)) {
+		throw std::exception("Failed to get console info");
+	}
+}
+
+int ConsoleManager::getConsoleWidth() {
+	loadConsoleInfo();
+	return csbi.srWindow.Right - csbi.srWindow.Left + 1;
+}
+int ConsoleManager::getConsoleHeight() {
+	loadConsoleInfo();
+	return csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+}
+
+bool ConsoleManager::setCConsoleWindowTitle(const char* title) {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	return SetConsoleTitle(title);
+}
+bool ConsoleManager::setConsoleWindowSize(short width, short height) {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	SMALL_RECT sr = { 0, 0, width - 1, height - 1 };
+	return SetConsoleWindowInfo(hConsole, TRUE, &sr);
+}
+bool ConsoleManager::setConsoleBufferSize(short width, short height) {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	COORD coord = { width, height };
+	return SetConsoleScreenBufferSize(hConsole, coord);
+}
+
+
+void ConsoleManager::loadCursorInfo() {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		throw std::exception("Invalid console handle");
+	}
+	if (!GetConsoleCursorInfo(hConsole, &cci)) {
+		throw std::exception("Failed to get cursor info");
+	}
+}
+
+bool ConsoleManager::isConsoleCursorVisible() {
+	loadCursorInfo();
+	return cci.bVisible;
+}
+int ConsoleManager::getConsoleCursorSize() {
+	loadCursorInfo();
+	return cci.dwSize;
+}
+
+bool ConsoleManager::setConsoleCursorVisible(bool visible) {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	cci = { cci.dwSize, visible };
+	return SetConsoleCursorInfo(hConsole, &cci);
+}
+bool ConsoleManager::setConsoleCursorSize(unsigned int size) {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	cci = { size, cci.bVisible };
+	return SetConsoleCursorInfo(hConsole, &cci);
+}
+
+COORD ConsoleManager::getConsoleCursorPosition() {
+	loadConsoleInfo();
+	return csbi.dwCursorPosition;
+}
+bool ConsoleManager::moveConsoleCursorPosition(short x, short y) {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	COORD coord = { x, y };
+	return SetConsoleCursorPosition(hConsole, coord);
+}
+bool ConsoleManager::clearConsole() {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	const COORD coord = { 0, 0 };
+	DWORD count;
+	return WriteConsoleOutputCharacterA(hConsole, " ", 1, coord, &count);
+}
+
+
+ConsoleManager::ConsoleManager() {
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		throw std::exception("Failed to get console handle");
+	}
+	loadConsoleInfo();
+	loadCursorInfo();
+}
+ConsoleManager::~ConsoleManager() {
+	if (hConsole != INVALID_HANDLE_VALUE) {
+		CloseHandle(hConsole);
+	}
+}
+
+
+
+
+
+bool ConsoleManager::setConsoleMode(DWORD mode) {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	return SetConsoleMode(hConsole, mode);
+}
+DWORD ConsoleManager::getConsoleMode() {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	DWORD mode;
+	GetConsoleMode(hConsole, &mode);
+	return mode;
+}
+
+
+
+bool ConsoleManager::readConsoleInput(INPUT_RECORD* buffer, DWORD& count) {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	return ReadConsoleInput(hConsole, buffer, count, &count);
+
+}
+
+
+bool ConsoleManager::setConsoleColor(WORD color) {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	loadConsoleInfo();
+	return SetConsoleTextAttribute(hConsole, color);
+}
+WORD ConsoleManager::getConsoleColor() {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	loadConsoleInfo();
+	return csbi.wAttributes;
+}
+bool ConsoleManager::resetConsoleColor() {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	return SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+}
+
+
+bool ConsoleManager::write(std::string str, WORD color, WORD bgColor) {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	DWORD count;
+	setConsoleColor(color|bgColor);
+	if (!WriteConsoleA(hConsole, str.c_str(), str.size(), &count, NULL)) {
+		resetConsoleColor();
+		return false;
+	}
+	resetConsoleColor();
+	return true;
+}
+
+
+bool ConsoleManager::writeOutput(RECT rect, const CHAR_INFO* buffer, COORD size, COORD start) {
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	return WriteConsoleOutput(hConsole, buffer, size, start, &csbi.srWindow);
+}
+bool ConsoleManager::writeOutput(SMALL_RECT rect, std::string str, COORD size, COORD pos, WORD Attributes){
+	if (hConsole == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+	CHAR_INFO* buffer = new CHAR_INFO[str.size()];
+	for (int i = 0; i < str.size(); i++) {
+		buffer[i].Char.UnicodeChar = str[i];
+		buffer[i].Attributes = Attributes;
+	}
+	WriteConsoleOutput(hConsole, buffer, size, pos, &rect);
+	delete[] buffer;
+	return true;
+}
+DWORD ConsoleManager::writeAttribute(WORD* attribute, int size, COORD pos){
+	DWORD numWritten;
+	WriteConsoleOutputAttribute(hConsole, attribute, size, pos, &numWritten);
+	return numWritten;
+}
+DWORD ConsoleManager::writeCharacter(std::string str, COORD pos) {
+	DWORD numWritten;
+	WriteConsoleOutputCharacter(hConsole, str.c_str(), str.size(), pos, &numWritten);
+	return numWritten;
+}
+
+
+std::string ConsoleManager::read() {
+	std::string str;
+	char c;
+	while (ReadConsole(hConsole, &c, 1, NULL, NULL)) {
+		if (c == '\r') {
+			continue;
+		}
+		if (c == '\n') {
+			break;
+		}
+		str += c;
+	}
+	return str;
+}
+std::string ConsoleManager::read(int size) {
+	std::string str;
+	char c;
+	for (int i = 0; i < size; i++) {
+		ReadConsole(hConsole, &c, 1, NULL, NULL);
+		str += c;
+	}
+	return str;
+}
+CHAR_INFO* ConsoleManager::readOutput(COORD size, COORD pos, SMALL_RECT rect) {
+	CHAR_INFO* buffer = new CHAR_INFO[size.X * size.Y];
+	ReadConsoleOutput(hConsole, buffer, size, pos, &rect);
+	return buffer;
+}
+WORD* ConsoleManager::readAttribute(COORD size, COORD pos) {
+	WORD* attribute = new WORD[size.X * size.Y];
+	ReadConsoleOutputAttribute(hConsole, attribute, size.X * size.Y, pos, NULL);
+	return attribute;
+}
+std::string ConsoleManager::readCharacter(COORD size, COORD pos) {
+	std::string str;
+	char* buffer = new char[size.X * size.Y];
+	ReadConsoleOutputCharacter(hConsole, buffer, size.X * size.Y, pos, NULL);
+	for (int i = 0; i < size.X * size.Y; i++) {
+		str += buffer[i];
+	}
+	delete[] buffer;
+	return str;
+}
+
+
+
+ConsoleButton::ConsoleButton() {}
+ConsoleButton::ConsoleButton(COORD pos, WORD width, WORD height) {
+	this->pos = pos;
+	this->width = width;
+	this->height = height;
+}
+void ConsoleButton::setPos(COORD pos) {
+	this->pos = pos;
+}
+void ConsoleButton::setWidth(WORD width) {
+	this->width = width;
+}
+void ConsoleButton::setHeight(WORD height) {
+	this->height = height;
+}
+COORD ConsoleButton::getPos() {
+	return pos;
+}
+WORD ConsoleButton::getWidth() {
+	return width;
+}
+WORD ConsoleButton::getHeight() {
+	return height;
+}
+bool ConsoleButton::isSelected() {
+	INPUT_RECORD buffer;
+	DWORD count;
+	if (!ConsoleM.readConsoleInput(&buffer, count)) {
+		return false;
+	}
+	if (buffer.EventType == MOUSE_EVENT) {
+		MOUSE_EVENT_RECORD mouseEvent = buffer.Event.MouseEvent;
+		if (mouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED) {
+			if (mouseEvent.dwMousePosition.X >= pos.X
+				&& mouseEvent.dwMousePosition.X <= pos.X + width
+				&& mouseEvent.dwMousePosition.Y >= pos.Y
+				&& mouseEvent.dwMousePosition.Y <= pos.Y + height) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+
+
+ConsoleButtonText::ConsoleButtonText(std::string text, COORD pos, WORD color, WORD bgColor) {
+	this->text = text;
+	this->pos = pos;
+	this->color = color;
+	this->bgColor = bgColor;
+	this->selected = false;
+	button = ConsoleButton(pos, short(text.size()), 1);
+}
+void ConsoleButtonText::setPos(COORD pos) {
+	this->pos = pos;
+}
+void ConsoleButtonText::setColor(WORD color) {
+	this->color = color;
+}
+void ConsoleButtonText::setBgColor(WORD bgColor) {
+	this->bgColor = bgColor;
+}
+void ConsoleButtonText::setText(std::string text) {
+	this->text = text;
+}
+COORD ConsoleButtonText::getPos() {
+	return pos;
+}
+WORD ConsoleButtonText::getColor() {
+	return color;
+}
+WORD ConsoleButtonText::getBgColor() {
+	return bgColor;
+}
+bool ConsoleButtonText::isSelected() {
+	if (button.isSelected()) {
+		selected = true;
+		return true;
+	}
+	selected = false;
+	return false;
+}
+void ConsoleButtonText::draw(){
+	if (selected) {
+		ConsoleM.setConsoleColor(color | 0x0080);
+	}
+	else {
+		ConsoleM.setConsoleColor(color|bgColor);
+	}
+	ConsoleM.writeOutput(SMALL_RECT{ pos.X, pos.Y, short(pos.X + text.size() - 1), pos.Y }, text, { short(text.size()), 1 }, COORD{ 0, 0 }, color | bgColor);
+}
---
+++ ConsoleManager.h
@@ -0,0 +1,175 @@
+#pragma once
+#include "Library.h"
+
+#define COLOR_RED (FOREGROUND_RED | FOREGROUND_INTENSITY)                                     
+#define COLOR_GREEN (FOREGROUND_GREEN | FOREGROUND_INTENSITY)                                 
+#define COLOR_BLUE (FOREGROUND_BLUE | FOREGROUND_INTENSITY)                                         
+#define COLOR_YELLOW (FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY)                      
+#define COLOR_MAGENTA (FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY)                        
+#define COLOR_CYAN (FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY)                       
+#define COLOR_WHITE (FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE)
+#define COLOR_BLACK (0)                                                                                                        
+
+#define BG_COLOR_RED (BACKGROUND_RED | BACKGROUND_INTENSITY)                                  
+#define BG_COLOR_GREEN (BACKGROUND_GREEN | BACKGROUND_INTENSITY)                               
+#define BG_COLOR_BLUE (BACKGROUND_BLUE | BACKGROUND_INTENSITY)	                                    
+#define BG_COLOR_YELLOW (BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_INTENSITY)                
+#define BG_COLOR_MAGENTA (BACKGROUND_RED | BACKGROUND_BLUE | BACKGROUND_INTENSITY)                
+#define BG_COLOR_CYAN (BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY)                                         
+#define BG_COLOR_WHITE (BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE)
+#define BG_COLOR_BLACK (0)                                                                              
+
+namespace ConsoleFactory {
+	class ConsoleManager
+	{
+	public:
+#ifdef NEW_CHAR_INFO
+		class CharInfo
+		{
+		private:
+			int x;
+			int y;
+			CHAR_INFO** ci;
+		public:
+			CharInfo();
+			CharInfo(int x, int y);
+		public:
+			bool writeFill(std::string str, WORD attr = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+			CHAR_INFO* getCharInfo();
+		};
+#endif
+	private:
+		HANDLE hConsole;
+
+		CONSOLE_SCREEN_BUFFER_INFO csbi;
+		CONSOLE_CURSOR_INFO cci;
+	public:
+		//获取窗口句柄
+		HANDLE getConsoleHandle();
+	public:
+		//读取窗口信息
+		void loadConsoleInfo();
+	public:
+	public:
+		//获取窗口宽度
+		int getConsoleWidth();
+		//获取窗口高度
+		int getConsoleHeight();
+		//设置窗口标题
+		bool setCConsoleWindowTitle(const char* title);
+		//设置窗口大小
+		bool setConsoleWindowSize(short width, short height);
+		//设置缓冲区大小
+		bool setConsoleBufferSize(short width, short height);
+
+	public: //cursor 
+		//读取光标信息
+		void loadCursorInfo();
+		//获取光标是否可见
+		bool isConsoleCursorVisible();
+		//获取光标大小
+		int getConsoleCursorSize();
+		//设置光标可见
+		bool setConsoleCursorVisible(bool visible);
+		//设置光标大小
+		bool setConsoleCursorSize(unsigned int size);
+
+		//获取光标位置
+		COORD getConsoleCursorPosition();
+		//移动光标位置
+		bool moveConsoleCursorPosition(short x, short y);
+		//清除屏幕
+		bool clearConsole();
+
+
+
+	public: //color
+		//获取颜色
+		WORD getConsoleColor();
+		//设置颜色
+		bool setConsoleColor(WORD color);
+		//重置颜色
+		bool resetConsoleColor();
+
+	public:
+		bool write(std::string str, WORD color = COLOR_WHITE, WORD bgColor = BG_COLOR_BLACK);
+
+		bool writeOutput(SMALL_RECT rect, std::string str, COORD size, COORD pos, WORD Attributes = COLOR_WHITE | BG_COLOR_BLACK);
+		bool writeOutput(RECT rect, const CHAR_INFO* buffer, COORD size, COORD start);
+		DWORD writeAttribute(WORD* attribute, int size, COORD pos);
+		DWORD writeCharacter(std::string str, COORD pos);
+
+		//读取相关
+	public:
+		std::string read();
+		std::string read(int size);
+		CHAR_INFO* readOutput(COORD size, COORD pos, SMALL_RECT rect);
+		WORD* readAttribute(COORD size, COORD pos);
+		std::string readCharacter(COORD size, COORD pos);
+
+	public: //input&output mode
+		//设置输入输出模式
+		bool setConsoleMode(DWORD mode);
+		//获取输入输出模式
+		DWORD getConsoleMode();
+
+
+		//读取总输入
+		bool readConsoleInput(INPUT_RECORD* buffer, DWORD& count);
+#ifdef DETAILED_INPUT_OUTPUT_INFO
+		//读取键盘输入
+		bool readConsoleKeyboardInput(INPUT_RECORD* buffer, DWORD& count);
+		//读取鼠标输入
+		bool readConsoleMouseInput(INPUT_RECORD* buffer, DWORD& count);
+		//读取窗口消息
+		bool readConsoleWindowMessage(LPMSG buffer, DWORD& count);
+#endif
+
+	public:
+		ConsoleManager();
+		~ConsoleManager();
+	};
+	class ConsoleButton {
+	private:
+		COORD pos;
+		WORD width;
+		WORD height;
+		WORD color;
+		WORD bgColor;
+	public:
+		ConsoleButton();
+		ConsoleButton(COORD pos, WORD width, WORD height);
+		void setPos(COORD pos);
+		void setWidth(WORD width);
+		void setHeight(WORD height);
+		COORD getPos();
+		WORD getWidth();
+		WORD getHeight();
+		//检测区域内是否按下
+		bool isSelected();
+	};
+	class ConsoleButtonText {
+	private:
+		std::string text;
+		COORD pos;
+		WORD color;
+		WORD bgColor;
+		bool selected;
+		ConsoleButton button;
+	public:
+		ConsoleButtonText(std::string text, COORD pos, WORD color = COLOR_WHITE, WORD bgColor = BG_COLOR_BLACK);
+		void setPos(COORD pos);
+		void setColor(WORD color);
+		void setBgColor(WORD bgColor);
+		void setText(std::string text);
+		COORD getPos();
+		WORD getColor();
+		WORD getBgColor();
+		//检测按钮是否按下
+		bool isSelected();
+		//绘制按钮
+		void draw();
+	};
+
+	static ConsoleManager ConsoleM;
+}
